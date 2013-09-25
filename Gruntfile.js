8419630cd5ac9ced1e5016648e2d81287d02ea99
Florian Uhlig <florianuhlig@outlook.com>
2013-09-25 23:17:09 +0200

    add initial structure

---
+++ Gruntfile.js
@@ -0,0 +1,199 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON( 'package.json' ),
+
+    watch: {
+      scss: {
+        files: ['scss/**/*.scss'],
+        tasks: 'scss'
+      },
+      html: {
+        files: ['src/**/*.hbs'],
+        tasks: 'html'
+      },
+      js: {
+        files: ['js/**/*.js'],
+        tasks: 'js'
+      },
+      livereload: {
+        options: {
+          livereload: true,
+          debounceDelay: 500
+        },
+        files: [
+          'dist/**/*.html',
+          'dist/assets/css/{,*/}*.css',
+          'dist/assets/js/{,*/}*.js'
+        ]
+      }
+    },
+
+    // GRUNT-SASS
+    // sass: {
+    //   build: {
+    //     files : [
+    //       {
+    //         src : ['**/*.scss', '!**/_*.scss'],
+    //         cwd : 'scss',
+    //         dest : 'css',
+    //         ext : '.css',
+    //         expand : true
+    //       }
+    //     ],
+    //     options : {
+    //       style : 'expanded'
+    //     }
+    //   }
+    // },
+
+    // GRUNT-CONTRIB-SASS
+    // sass: {
+    //   dist: {
+    //     options: {
+    //       sourcemap: true,
+    //       style: 'compressed'
+    //     },
+
+    //     files: [
+    //       {
+    //         expand: true,
+    //         cwd: 'scss',
+    //         dest: 'css',
+    //         ext: '.css',
+    //         src: ['**/*.scss', '!**/_*.scss']
+    //       }
+    //     ]
+    //   }
+    // },
+
+    // compass: {
+    //   dist: {
+    //     options: {
+    //       sassDir: 'scss',
+    //       cssDir: 'css'
+    //     }
+    //   }
+    // },
+
+    autoshot: {
+      default_options: {
+        options: {
+          // necessary config
+          path: 'screenshots/',
+          filename: '',
+          type: 'png',
+          // optional config, must set either remote or local
+          remote: 'http://localhost:<%= connect.options.port &>',
+          viewport: [
+            '800x600',
+            '1024x768',
+            '1920x1080'
+          ]
+        },
+      },
+    },
+
+    // https://github.com/nDmitry/grunt-autoprefixer
+    autoprefixer: {
+      build: {
+        options: {
+          browsers: ['last 2 versions', '> 1%']
+        },
+        files: [
+          {
+            src : ['**/*.css', '!**/*autoprefixed.css'],
+            cwd : 'css',
+            dest : 'css',
+            ext : '.autoprefixed.css',
+            expand : true
+          }
+        ]
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          // change this to '0.0.0.0' or '*' to access the server from outside
+          hostname: '0.0.0.0',
+          base: './'
+        }
+      }
+    },
+
+    open: {
+      dev: {
+        path: 'http://localhost:8000'
+      }
+    },
+
+    // compiles handlebars templates
+    assemble: {
+      options: {
+        flatten: true,
+        layout: 'layout.hbs',
+        layoutdir: 'src/templates/layouts',
+        assets: 'dist/assets',
+        partials: ['src/templates/pages/*.hbs', 'src/templates/parts/*.hbs']
+      },
+      demo: {
+        options: {
+          data: ['src/data/*.{json,yml}']
+        },
+        files: {
+          'dist/': ['src/templates/pages/*.hbs']
+        }
+      }
+    },
+
+    copy: {
+      demo: {
+        files: [
+          { expand: true, cwd: './css', src: ['./**/*.*'], dest: 'dist/assets/css' },
+          { expand: true, cwd: './js', src: ['./**/*.*'], dest: 'dist/assets/js' }
+        ]
+      },
+      css: {
+        files: [
+          { expand: true, cwd: './css', src: ['./**/*.*'], dest: 'dist/assets/css' }
+        ]
+      },
+      js: {
+        files: [
+          { expand: true, cwd: './js', src: ['./**/*.*'], dest: 'dist/assets/js' }
+        ]
+      },
+      js_vendor: {
+        files: [
+          { expand: true, cwd: './bower_components/jquery', src: ['jquery.js'], dest: 'js/vendor' },
+          { expand: true, cwd: './bower_components/modernizr', src: ['modernizr.js'], dest: 'js/vendor' }
+        ]
+      }
+    }
+
+  });
+
+  // Default task
+  grunt.registerTask('default', [
+    // 'compass',
+    'assemble', 'copy']);
+
+  grunt.registerTask('scss', [
+    // 'compass',
+    'copy:css']);
+  grunt.registerTask('html', ['assemble']);
+  grunt.registerTask('js', ['copy:js']);
+
+  grunt.registerTask('dev', ['connect', 'watch', 'open:dev']);
+  grunt.registerTask('demo', ['copy:demo', 'assemble:demo', 'open']);
+  grunt.registerTask('deploy', ['gh-pages']);
+
+  grunt.registerTask('screenshots', ['autoshot:default_options']);
+
+  grunt.loadNpmTasks('assemble');
+  grunt.loadNpmTasks('grunt-notify');
+  grunt.loadNpmTasks('grunt-devtools');
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+};
---
+++ watch.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# ** watches and compiles sass/scss files **
+
+# modified version from @csswizardry watch script
+# https://github.com/csswizardry/inuit.css-web-template/blob/master/css/watch
+
+# generate sourcemap (sass 3.3 required [gem install sass --pre])
+sass --watch --sourcemap scss:css
+
+# No minification
+# sass --watch scss:css --style expanded
+
+# No minification
+# sass --watch scss:css --style compressed
+
+exit 0
